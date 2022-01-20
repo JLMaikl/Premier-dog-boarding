032b1cf390b6d4e238071a2f51564b9973372a3c
Alice <alice@mail.ru>
2022-01-20 09:58:20 +0300

    Added drop-down menu

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const iconServise = document.querySelector('.icon-servise');
+const downlistServise = document.querySelector('.menu-downlist_servise');
+const iconShop = document.querySelector('.icon-shop');
+const downlistShop = document.querySelector('.menu-downlist_shop');
+
+iconServise.addEventListener('click', () => {    
+    downlistServise.classList.toggle('menu-downlist_hidden')
+})
+
+iconShop.addEventListener('click', () => {    
+    downlistShop.classList.toggle('menu-downlist_hidden')
+})
