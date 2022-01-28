73b0d23c8ab568b573cd1b18ee2ae5da9363bf87
Alice <alice@mail.ru>
2022-01-28 12:42:56 +0300

    Add apointment blok with form

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const iconServise = document.querySelector('.icon-servise');
 const downlistServise = document.querySelector('.menu-downlist_servise');
 const iconShop = document.querySelector('.icon-shop');
 const downlistShop = document.querySelector('.menu-downlist_shop');
+const form = document.querySelector('.form');
 
 iconServise.addEventListener('click', () => {    
     downlistServise.classList.toggle('menu-downlist_hidden')
@@ -10,3 +11,7 @@ iconServise.addEventListener('click', () => {
 iconShop.addEventListener('click', () => {    
     downlistShop.classList.toggle('menu-downlist_hidden')
 })
+
+// form.addEventListener('click', (event) => {
+//     event.preventDefault()
+// })
