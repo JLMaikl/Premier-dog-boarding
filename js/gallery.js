fe292e4a9527c498cecb9e7e6d7f68d7cf3bf37a
Alice <alice@mail.ru>
2022-01-31 12:20:23 +0300

    Made face of page

---
+++ js/gallery.js
@@ -0,0 +1,13 @@
+const galleryScroll = () => {
+
+    const galleryOne = document.querySelector('.info-item_one')
+    const galleryTwo = document.querySelector('.info-item_one')
+    const galleryThree = document.querySelector('.info-item_one')
+    const photoOne = document.querySelector('.info-photo_one')
+    const photoTwo = document.querySelector('.info-photo_two')
+    const photoThree = document.querySelector('.info-photo_three')
+
+
+}
+
+galleryScroll()
